cd68bf81ccd2620cc18536bddcc4eb895ffbad3e
Muhammad Fadli <paddle12@users.noreply.huggingface.co>
2024-06-07 12:17:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,97 @@
 import cv2
 import gradio as gr
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras.preprocessing import image
-from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 
-# Load the pre-trained models
-model_path = 'model/densenet.hdf5'
-pretrained_model_path = 'model/pretrained_model.h5'
-model = load_model(model_path)
-pretrained_model = load_model(pretrained_model_path)
+class GradCAM:
+    def __init__(self, model, layer_name):
+        self.model = model
+        self.layer_name = layer_name
+        self.grad_model = tf.keras.models.Model(
+            [self.model.inputs],
+            [self.model.get_layer(layer_name).output, self.model.output]
+        )
+
+    def __call__(self, img_array, cls):
+        with tf.GradientTape() as tape:
+            conv_outputs, predictions = self.grad_model(img_array)
+            loss = predictions[:, cls]
+
+        output = conv_outputs[0]
+        grads = tape.gradient(loss, conv_outputs)[0]
+        gate_f = tf.cast(output > 0, 'float32')
+        gate_r = tf.cast(grads > 0, 'float32')
+        guided_grads = gate_f * gate_r * grads
+
+        weights = tf.reduce_mean(guided_grads, axis=(0, 1))
+
+        cam = np.zeros(output.shape[0:2], dtype=np.float32)
+
+        for index, w in enumerate(weights):
+            cam += w * output[:, :, index]
+
+        cam = cv2.resize(cam.numpy(), (224, 224))
+        cam = np.maximum(cam, 0)
+        cam = cam / cam.max()
+
+        return cam
+
+def apply_heatmap(img, heatmap, heatmap_ratio=0.6):
+    heatmap = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
+    heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+    return np.uint8(heatmap * heatmap_ratio + img * (1 - heatmap_ratio))
+
+def load_image(img_path, df, preprocess=True, H=320, W=320):
+    mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
+    x = image.load_img(img_path, target_size=(H, W))
+    x = image.img_to_array(x)
+    if preprocess:
+        x -= mean
+        x /= std
+        x = np.expand_dims(x, axis=0)
+    return x
+
+def get_mean_std_per_batch(image_path, df, H=320, W=320):
+    sample_data = []
+    for idx, img in enumerate(df.sample(100)["Image Index"].values):
+        sample_data.append(
+            np.array(image.load_img(image_path, target_size=(H, W))))
+    mean = np.mean(sample_data[0])
+    std = np.std(sample_data[0])
+    return mean, std
+
+def compute_gradcam(img, model, df, labels, layer_name='bn'):
+    preprocessed_input = load_image(img, df)
+    predictions = model.predict(preprocessed_input)
+
+    top_indices = np.argsort(predictions[0])[-3:][::-1]
+    top_labels = [labels[i] for i in top_indices]
+    top_predictions = [predictions[0][i] for i in top_indices]
+
+    original_image = load_image(img, df, preprocess=False)
+
+    grad_cam = GradCAM(model, layer_name)
+
+    gradcam_images = []
+    for i in range(3):
+        idx = top_indices[i]
+        label = top_labels[i]
+        prob = top_predictions[i]
+
+        gradcam = grad_cam(preprocessed_input, idx)
+        gradcam_image = apply_heatmap(original_image, gradcam)
+        gradcam_images.append((gradcam_image, f"{label}: p={prob:.3f}"))
+
+    return gradcam_images
+
+def gradio_interface_gradcam(img, df, labels, model_path, pretrained_model_path, layer_name='bn'):
+    model = load_model(model_path)
+    pretrained_model = load_model(pretrained_model_path)
+    labels = labels.split(',')
+    gradcam_images = compute_gradcam(img, pretrained_model, df, labels, layer_name)
+    return [gr.Image.update(value=image, label=label) for image, label in gradcam_images]
 
 def calculate_mse(original_image, enhanced_image):
     mse = np.mean((original_image - enhanced_image) ** 2)
@@ -33,24 +114,20 @@ def calculate_l2rat(original_image, enhanced_image):
     return l2norm_ratio
 
 def process_image(original_image, enhancement_type, fix_monochrome=True):
-    # Convert image to grayscale if desired
     if fix_monochrome:
         original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    
-    # Normalize the image (assuming it's already a NumPy array)
+
     image = original_image - np.min(original_image)
     image = image / np.max(original_image)
     image = (image * 255).astype(np.uint8)
-    
-    # Enhance the image based on selection
+
     enhanced_image = enhance_image(image, enhancement_type)
 
-    # Calculate image quality metrics
     mse = calculate_mse(original_image, enhanced_image)
     psnr = calculate_psnr(original_image, enhanced_image)
     maxerr = calculate_maxerr(original_image, enhanced_image)
     l2rat = calculate_l2rat(original_image, enhanced_image)
-    
+
     return enhanced_image, mse, psnr, maxerr, l2rat
 
 def apply_clahe(image):
@@ -89,89 +166,6 @@ def enhance_image(image, enhancement_type):
     else:
         raise ValueError(f"Unknown enhancement type: {enhancement_type}")
 
-def get_mean_std_per_batch(image_path, df, H=320, W=320):
-    sample_data = []
-    for idx, img in enumerate(df.sample(100)["Image Index"].values):
-        sample_data.append(
-            np.array(image.load_img(image_path, target_size=(H, W))))
-    mean = np.mean(sample_data[0])
-    std = np.std(sample_data[0])
-    return mean, std
-
-def load_image(img_path, df, preprocess=True, H=320, W=320):
-    mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
-    x = image.load_img(img_path, target_size=(H, W))
-    x = image.img_to_array(x)
-    if preprocess:
-        x -= mean
-        x /= std
-        x = np.expand_dims(x, axis=0)
-    return x
-
-def grad_cam(input_model, img_array, cls, layer_name):
-    grad_model = tf.keras.models.Model(
-        [input_model.inputs],
-        [input_model.get_layer(layer_name).output, input_model.output]
-    )
-
-    with tf.GradientTape() as tape:
-        conv_outputs, predictions = grad_model(img_array)
-        loss = predictions[:, cls]
-
-    output = conv_outputs[0]
-    grads = tape.gradient(loss, conv_outputs)[0]
-    gate_f = tf.cast(output > 0, 'float32')
-    gate_r = tf.cast(grads > 0, 'float32')
-    guided_grads = gate_f * gate_r * grads
-
-    weights = tf.reduce_mean(guided_grads, axis=(0, 1))
-
-    cam = np.zeros(output.shape[0:2], dtype=np.float32)
-
-    for index, w in enumerate(weights):
-        cam += w * output[:, :, index]
-
-    cam = cv2.resize(cam.numpy(), (224, 224))
-    cam = np.maximum(cam, 0)
-    cam = cam / cam.max()
-
-    return cam
-
-def compute_gradcam(img, model, df, labels, layer_name='bn'):
-    preprocessed_input = load_image(img, df)
-    predictions = model.predict(preprocessed_input)
-
-    top_indices = np.argsort(predictions[0])[-3:][::-1]
-    top_labels = [labels[i] for i in top_indices]
-    top_predictions = [predictions[0][i] for i in top_indices]
-
-    plt.figure(figsize=(20, 20))
-    plt.subplot(4, 4, 1)
-    plt.title("Original Image")
-    plt.axis('off')
-    original_image = load_image(img, df, preprocess=False)
-    plt.imshow(original_image, cmap='gray')
-
-    for i in range(3):
-        idx = top_indices[i]
-        label = top_labels[i]
-        prob = top_predictions[i]
-
-        gradcam = grad_cam(model, preprocessed_input, idx, layer_name)
-
-        plt.subplot(4, 4, i+2)
-        plt.title(f"{label}: p={prob:.3f}")
-        plt.axis('off')
-        plt.imshow(original_image, cmap='gray')
-        plt.imshow(gradcam, cmap='jet', alpha=0.5)
-
-    plt.show()
-
-def gradio_interface_gradcam(img, df, labels, model):
-    labels = labels.split(',')
-    compute_gradcam(img, model, df, labels)
-    return img
-
 iface = gr.Interface(
     fn=process_image,
     inputs=[
@@ -194,15 +188,18 @@ iface_gradcam = gr.Interface(
         gr.Image(type="numpy", label="Upload Image for Grad-CAM"),
         gr.Dataframe(label="Dataframe for Mean/Std Calculation"),
         gr.Textbox(label="Labels", lines=5, placeholder="Enter labels separated by commas"),
-        gr.Model(label="Model")
+        gr.Textbox(label="Model Path", default='model/densenet.hdf5'),
+        gr.Textbox(label="Pretrained Model Path", default='model/pretrained_model.h5')
     ],
     outputs=[
-        gr.Plot(label="Grad-CAM Visualization")
+        gr.Image(label="Grad-CAM 1"),
+        gr.Image(label="Grad-CAM 2"),
+        gr.Image(label="Grad-CAM 3")
     ],
     title="Grad-CAM Visualization"
 )
 
-# Combine interfaces
 iface_combined = gr.TabbedInterface([iface, iface_gradcam], ["Image Enhancement and Quality Evaluation", "Grad-CAM Visualization"])
 
-iface_combined.launch()
+if __name__ == "__main__":
+    iface_combined.launch()
